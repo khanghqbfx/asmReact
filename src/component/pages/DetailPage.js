a4e28fc272af95fd149f8fb1641b9c079ef7ee44
khanghqbfx <128677676+khanghqbfx@users.noreply.github.com>
2023-08-09 09:36:13 +0700

    Add files via upload

--- src/component/pages/DetailPage.js
+++ src/component/pages/DetailPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Detail from '../DeltaiPage/Detail';
-
+import MainNavigation from '../MainNavigation/MainNavigation';
+import HeaderDeital from '../DeltaiPage/Header';
+import Footer from '../Footer/Footer';
 
 
 function DetailPage(props) {
   return (
     <div>
+      <MainNavigation />
+      <HeaderDeital />
       <Detail />
+      <Footer />
     </div>
   );
 }
